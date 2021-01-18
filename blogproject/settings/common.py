ae603b8851ec5961465a0e6eacc8cafe68cc36c0
QIUZHENWANG <1060373463@qq.com>
2021-01-18 23:24:19 +0800

    use_fabric

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -13,19 +13,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*r%qai8-l&011_g(z=5-xdah4t7umzkn@8sqi8w)7wy_bo4&s6'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.guanzhe.top', '.qiuzhenwang.top']
 
 
 # Application definition
---
+++ blogproject/settings/production.py
@@ -0,0 +1,9 @@
+form .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*r%qai8-l&011_g(z=5-xdah4t7umzkn@8sqi8w)7wy_bo4&s6'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.guanzhe.top', '.qiuzhenwang.top']
