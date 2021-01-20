5125f9aa03ba92eff75c87207c2df177a876b880
QIUZHENWANG <1060373463@qq.com>
2021-01-21 01:00:59 +0800

    'auto_commit'

--- fabfile.py
+++ fabfile.py
@@ -61,3 +61,14 @@ def test(c):
     con = Connection(host_ip, user_name, connect_kwargs={'password': password})
     result = con.run(cmd, hide=True)
     print(result)
+
+@task()
+def fastg(c):
+    git_path = "G:\python_project\HelloDjango-blog-tutorial"
+    with c.cd(git_path):
+        print("1.")
+        cmd = 'git add .'
+        c.run(cmd)
+        cmd = "git commit -m 'auto_commit'"
+        c.run(cmd)
+        cmd = "git push origin server_version"
